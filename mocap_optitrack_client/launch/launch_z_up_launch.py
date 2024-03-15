1f8d2e5fa80986dc137597c77ba2c748ce7d53e8
Elvin Yang <eyy@umich.edu>
2024-03-15 14:34:49 -0400

    Turn off extra nodes

--- mocap_optitrack_client/launch/launch_z_up_launch.py
+++ mocap_optitrack_client/launch/launch_z_up_launch.py
@@ -5,8 +5,10 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    log_level = "warn"
+
     ld = LaunchDescription()
-    #Create the NatNet client node
+    # Create the NatNet client node
     config = os.path.join(
         get_package_share_directory('mocap_optitrack_client'),
         'config',
@@ -19,34 +21,34 @@ def generate_launch_description():
         parameters = [config],
         arguments=['--ros-args', '--log-level', log_level]
     )
-#Create the world to base client
-    config = os.path.join(
-        get_package_share_directory('mocap_optitrack_w2b'),
-        'config',
-        'world_to_base_z_up.yaml'
-    )
-    world_to_base = Node(
-        package='mocap_optitrack_w2b',
-        executable='mocap_optitrack_w2b',
-        name='world_to_base',
-        parameters = [config],
-        arguments=['--ros-args', '--log-level', log_level]
-    )
-    #Create the inverse kinematics node
-    config = os.path.join(
-        get_package_share_directory('mocap_optitrack_inv_kin'),
-        'config',
-        'inverse_kinematics.yaml'
-    )
-    inverse_kinematics = Node(
-        package='mocap_optitrack_inv_kin',
-        executable='mocap_optitrack_inv_kin',
-        name='inverse_kinematics',
-        parameters = [config],
-        arguments=['--ros-args', '--log-level', log_level]
-    )
-    #Add the nodes to the launch description and return it
+    # Create the world to base client
+    # config = os.path.join(
+    #     get_package_share_directory('mocap_optitrack_w2b'),
+    #     'config',
+    #     'world_to_base_z_up.yaml'
+    # )
+    # world_to_base = Node(
+    #     package='mocap_optitrack_w2b',
+    #     executable='mocap_optitrack_w2b',
+    #     name='world_to_base',
+    #     parameters = [config],
+    #     arguments=['--ros-args', '--log-level', log_level]
+    # )
+    # Create the inverse kinematics node
+    # config = os.path.join(
+    #     get_package_share_directory('mocap_optitrack_inv_kin'),
+    #     'config',
+    #     'inverse_kinematics.yaml'
+    # )
+    # inverse_kinematics = Node(
+    #     package='mocap_optitrack_inv_kin',
+    #     executable='mocap_optitrack_inv_kin',
+    #     name='inverse_kinematics',
+    #     parameters = [config],
+    #     arguments=['--ros-args', '--log-level', log_level]
+    # )
+    # Add the nodes to the launch description and return it
     ld.add_action(natnet_client)
-    ld.add_action(world_to_base)
-    ld.add_action(inverse_kinematics)
+    # ld.add_action(world_to_base)
+    # ld.add_action(inverse_kinematics)
     return ld
